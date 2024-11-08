5a55e1abb44dc359536913fa5c0e3eae19d0f659
Daniel-C-Igbokwe <107478740+Daniel-C-Igbokwe@users.noreply.github.com>
2024-11-07 22:17:22 -0600

    Learning react in this crash course

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import HomeCards from "./components/HomeCards.jsx"
+import JobListings from "./components/JobListings.jsx"
+import ViewAllJobs from "./components/ViewAllJobs.jsx"
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  )
+}
+
+export default App
