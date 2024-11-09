a750cc9da4fab3c596aac4a98bf84731c1cb6a06
Daniel-C-Igbokwe <107478740+Daniel-C-Igbokwe@users.noreply.github.com>
2024-11-08 22:51:16 -0600

    move on to other works

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import HomeCards from "./components/HomeCards.jsx"
-import JobListings from "./components/JobListings.jsx"
-import ViewAllJobs from "./components/ViewAllJobs.jsx"
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
+
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  )
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />}/>
+        <Route path='/jobs' element={<JobsPage />}/>
+        
+        <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+        <Route path='*' element={<NotFoundPage />}/>
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={ router } />
 }
 
 export default App
